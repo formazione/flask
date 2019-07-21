4d7d382abae1a6713df7c37794e1b8a429b11bf3
Glitch (pythontutorial) <none>
2019-07-21 04:29:55 +0000

    🐵🍕 Updated with Glitch

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a template to create a blog with Flask and Python on glitch.com
+This was made to show how to import and export this code
+
+The video tutorial about this
+============
+
--- run.py
+++ run.py
@@ -3,37 +3,64 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# global variables
+
+
+menu = [
+  {"home" : "Home"}
+]
+
 @app.route("/")
-def homepage():  
+def homepage():
+  """The main page"""
+  title = "A free Blog with Python, Flask and Glitch"
   posts = [
     
-    {
-      "title" : "Welcome to my blog/tutorial",
-      "body" : "This blog was born to give you the essential knowledge to create a blog with Flask in glitch.com. You can take a look at my video on my channel."
-      
-    },
+    {"title" : "Welcome to my Python blog/tutorial",
+      "body" : "In this blog we will talk about basic stuffs about Python programming language."},
     
-    {"title": "1. How to use Python in Glitch",
-    "body": "First you need an account on glitch.com, then you create a new project."
-    },
-    {"title": "2. Your first flask blog",
-     "body": "First of all create a start.sh file with python run.py in it."
-    },
+    {"title" : "start.sh",
+    "body" : "This is the file that makes everything starts. It contains just this command: 'python run.py', where run.py is the second file you will create and it's the whole 'brain' of your site in Python."},
+    
+    {"title": "run.py",
+    "body" : "This is the main file, responsible for the site's behaviour. Here you can put all the data you need to be shown in the site, while the other file will just be needed to show the data in the desired way."},
     
     {
-      "title": "3. The run.py file",
-      "body": "In this  file you will import Flask and render_template from the module flask (that will be listed in the file requirement.txt (we will talk about it in the next post)). We will create an istance of Flask, named app. Then we will create the views. We will also launch the app.run() method to start the blog."
-    }
+      "title" : "Folder",
+      "body" : "We have two folders: static that contains css files and templates that contains html pages"
+    },
+    
+    {"title" : "Jinja",
+    "body" : "This module allows you to use the data from run.py into the rendered html pages of the site. When you use render_template, you pass as argument, the name of the html page and the variables you can use in the code of the page itself, as we will soon explain."}
+    
+
   ]
-  
-  return render_template("index.html", posts=posts)
+  showinfo = 1
+  return render_template("index.html",
+                         menu = menu,
+                         title=title,
+                         posts=posts,
+                         showinfo=showinfo)
 
 @app.route("/info")
 def info():
-  return """
-  <h2>I'm Giovanni....</h2>
-  <a href="/">Home</a>
-  """
+  """The info about myself page"""
+  showinfo = 0
+  title = "Info page"
+  posts = [
+  { 
+    "title" : "Info about me",
+    "body" : 
+    "Hi. I'm Giovanni Python. Subscribe to my channel on Youtube if you want more tutorial like this.",
+  }
+    
+  ]
+
+  return render_template("info.html",
+                         menu=menu,
+                         title = title,
+                         posts=posts,
+                         showinfo=showinfo)
 
 if __name__ == "__main__":
   app.run()
