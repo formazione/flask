f012f2708e7ad67d13a945fb24c868212fdc445a
Glitch (5ce) <none>
2019-09-18 04:45:15 +0000

    🦁🚐 Updated with Glitch

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 # global variables
 
 
@@ -13,25 +12,12 @@ menu = [
 @app.route("/")
 def homepage():
   """The main page"""
-  title = "A free Blog with Python, Flask and Glitch"
+  title = "Quinta"
+  image1 = "https://cdn.glitch.com/8c2f7a3d-e74c-4f36-8d5e-089c50dcc752%2FIS_tech_class_group.jpg?v=1568692442522"
   posts = [
     
-    {"title" : "Welcome to my Python blog/tutorial",
-      "body" : "In this blog we will talk about basic stuffs about Python programming language."},
-    
-    {"title" : "start.sh",
-    "body" : "This is the file that makes everything starts. It contains just this command: 'python run.py', where run.py is the second file you will create and it's the whole 'brain' of your site in Python."},
-    
-    {"title": "run.py",
-    "body" : "This is the main file, responsible for the site's behaviour. Here you can put all the data you need to be shown in the site, while the other file will just be needed to show the data in the desired way."},
-    
-    {
-      "title" : "Folder",
-      "body" : "We have two folders: static that contains css files and templates that contains html pages"
-    },
-    
-    {"title" : "Jinja",
-    "body" : "This module allows you to use the data from run.py into the rendered html pages of the site. When you use render_template, you pass as argument, the name of the html page and the variables you can use in the code of the page itself, as we will soon explain."}
+    {"title" : "Il Bilancio",
+      "body" : "Il bilancio e' formato da tre documenti: Stato patrimoniale, Conto economico e Nota integrativa."},
     
 
   ]
@@ -39,6 +25,7 @@ def homepage():
   return render_template("index.html",
                          menu = menu,
                          title=title,
+                         image1=image1,
                          posts=posts,
                          showinfo=showinfo)
 
@@ -51,7 +38,7 @@ def info():
   { 
     "title" : "Info about me",
     "body" : 
-    "Hi. I'm Giovanni Python. Subscribe to my channel on Youtube if you want more tutorial like this.",
+    "This is a blog for the 5ce students.",
   }
     
   ]
