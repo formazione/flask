b63a0739f228d1b2b0f4b770ddaefa750047178c
Glitch (pil) <none>
2019-10-03 07:18:00 +0000

    🔋📈 Updated with Glitch

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask import Markup
 import re
-
+from keyword import kwlist
 app = Flask(__name__)
 
 
@@ -12,6 +12,29 @@ menu = [
   {"home" : "Home"}
 ]
 
+
+def highlight(code):
+	"pass a string and it will be highlighted"
+	
+	# keywords to be colored in orange
+	kw = kwlist
+	for k in kw:
+		k = k + " "
+    
+		code = code.replace(k, "<b style='color:orange'>" + k + "</b>")
+	code = code.replace("\n","<br>")
+	#print(code)
+
+	# The 'indentation'
+	code = code.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;")
+
+	# functions to be clored in blue
+	_def= re.findall("\w+\(", code)
+	for w in _def:
+		code = code.replace(w, "<b style='color:blue'>" + w[:-1] + "</b>(")
+	return code
+
+
 def markup(posts):
   # bold instead of asteriscs
   for p in posts:
@@ -20,17 +43,45 @@ def markup(posts):
       p[k] = p[k].split(" ")
       print(p[k])
       for n,w in enumerate(p[k]):
-        if "*" in w:
+        if "." in w:
+          w = w.split(".")
+          for nn,sub in enumerate(w):
+            if "*" in sub:
+              sub = sub.replace("*","")
+              w[nn] = "<b>" + sub + "</b>"
+            if "^" in sub:
+              sub = sub.replace("^","")
+              w[nn] = "<b style='color:blue'>" + sub + "</b>"
+            if "#" in sub:
+              sub = sub.replace("#","")
+              w[nn] = "<b style='color:coral'>" + sub + "</b>"
+          w = ".".join(w)
+          p[k][n] = w
+        elif "*" in w:
           w = w.replace("*","")
           p[k][n] = "<b>"+w+"</b>"
+        elif "^" in w:
+          w = w.replace("^","")
+          p[k][n] = "<b style='color:blue'>"+w+"</b>"
+        elif "#" in w:
+          w = w.replace("#","")
+          p[k][n] = "<b style='color:coral'>"+w+"</b>"
       p[k] = " ".join(p[k])
   
   # Markup everything
   for post in posts:
-    for k in post:
-      post[k] = Markup(post[k])
+    testo = post["body"].splitlines()
+    for line in testo:
+      if line != "":
+        if line[0] == ">":
+          testo[testo.index(line)] = Markup(highlight(line))
+    testo = "".join(testo)
+    post["body"] = Markup(testo)
+    #post["body"] = Markup(highlight(post["body"]))
 
-  
+
+
+
       
 @app.route("/")
 def homepage():
@@ -48,49 +99,90 @@ We will take a fast look at the main functions of this very useful module."""},
     
     {"title" : "Install",
     "body" : """First you need to install pil's fork pillow:
-pip install pillow"""},
+pip install pillow
+<img src="https://cdn.glitch.com/8f79309f-0a88-41bb-bfd9-ccac7007a188%2Fpip_install_pil.PNG?v=1569514131173" />
+"""},
  
     {"title" : "Import",
-    "body" : """from PIL import Image"""},
+    "body" : """
+    The first thing you need to import is the Class Image
+    <br>
+>>> from PIL import Image"""},
+    
+    # CREATE A NEW IMAGE
     
     {"title" : "Create",
-    "body" : """img = Image.new('RBG', (600,400), 'yellow')"""},
+    "body" : """>>> img = Image.new('RBG', (200,200), 'yellow')
+<br><br>
+This will *create a yellow square image of 200 px of *width and 200 px of *height. *RGB is for Red, Green and Blue, aka the image is in color. If you want to use *transparency you have to use *RGBA instead of RGB.
+"""},
     
     {"title" : "Open",
-     "body" : """img = Image.open('existing.png')"""},
+     "body" : """>>> img = Image.open('existing.png')"""},
     
     {"title" : "Save",
-     "body" : """img.save('myimage.png')"""},
+     "body" : ">>> img.save('myimage.png')"},
     
     {"title" : "Show",
-     "body" : """img.show()"""},
+     "body" : ">>> img.show()"},
     
     {"title" : "Resize",
-     "body" : """img.resize((100,100), Image.ANTIALIAS)"""},
+     "body" : """>>> img.resize((100,100), Image.ANTIALIAS)"""},
     
-    {"title" : "Blur (from PIL import ImageFilter)",
-     "body" : """img.filter(ImageFilter.BLUR)"""},
+    {"title" : "Filters for the images",
+     "body" : """
+>>> img.filter(ImageFilter.BLUR)
+<br><br>
+There's a much smoother blur called SMOOTH
+<br>
+>>> i = i.filter(ImageFilter.SMOOTH )
+<br><br>
+You can then create a 'contour' effect
+<br>
+>>> i = i.filter( ImageFilter.CONTOUR )
+"""},
     
-    {"title": "A smooth blur",
-    "body" : 
-      """This is much softer than BLUR
-i = i.""filter(ImageFilter.SMOOTH)"""
-    },
+
   
     {"title" : "Blend 2 images together",
-     "body" : """img = Image.blend(Image.open('image1.png','image2.png', 0.5))"""},
+     "body" : """>>> img = Image.blend(Image.open('image1.png','image2.png', 0.5) )"""},
     
     {"title" : "Pasting an image on another",
-     "body" : """img.paste((0,0),'image2.png')"""},
+     "body" : """>>> img.paste((0,0),'image2.png')"""},
     
     {"title" : "Write text on an image (ImageDraw)",
-     "body" : """draw = ImageDraw.Draw(img)
-               draw.text(0,0,'This text goes on top of the image')"""},
+     "body" : """
+>>> draw = ImageDraw.Draw(img)
+<br>
+>>> draw.text(0,0,'This text goes on top of the image')"""},
     
     {
       
       "title" : "Thumbnail",
-      "body" : """im.thumbnail((128, 128), Image.ANTIALIAS)"""
+      "body" : """>>> im.thumbnail((128, 128), Image.ANTIALIAS)"""
+      
+    },
+    
+    
+    {
+      "title" : "text and font",
+      
+      "body" : """
+To add a *text to an *image after you created an image:
+<br>
+>>> im = img.new("RGBA", (600,400), "yellow")
+<br>
+You create a ^ImageDraw.Draw object to wich you pass the ^im object you created and on wich you draw:
+<br>
+>>> d = ImageDraw.Draw(im)
+<br><br>
+Now you are ready to add a text to the ^d object, but you can define the *size and *family of the text, using the ^ImageFont *class (that you need to import from PIL) together with the method ^truetype:
+<br>
+>>> font = ImageFont.truetype("Arial 20")
+<br><br>
+Now, you can finally add this font to the ^font ^argument when you add the text like this:
+>>> d.text((10,10), "text to show", (255,255,255), font=font)
+      """
       
     }
     
